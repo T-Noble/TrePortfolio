3a3fc22b29461fc3d324fc9c89bbee477bc27e28
T-Noble <trenob7641@students.ecpi.edu>
2025-01-11 00:12:24 -0500

    Personal photo added to site along with a description box

--- TrevonPortfolioSite/Controllers/HomeController.cs
+++ TrevonPortfolioSite/Controllers/HomeController.cs
@@ -23,6 +23,18 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Projects(){
+        return View();
+    }
+
+    public IActionResult SQL(){
+        return View();
+    }
+
+    public IActionResult Contacts(){
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
